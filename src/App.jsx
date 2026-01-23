09edd843c805d53cf33e87f09208adf26a1b8aec
sadupadasun362@gmail.com <sadupadasun@gmail.com>
2026-01-23 09:58:51 +0530

    Button Creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import SearchBar from './componencts/Molecules/SearchBar'
 import Footer from './componencts/Organisms/Footer';
-import Header from './componencts/Organisms/Header'
+import Header from './componencts/Organisms/Header';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <>
       <div className='app-container'>
         <Header/>
+          <main className='content'>
+            <Home/>
+          </main>
         <Footer/>
       </div>
     </>
